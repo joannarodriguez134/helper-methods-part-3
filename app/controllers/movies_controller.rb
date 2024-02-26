3603f0624158214b143ede433680a07db539ef2e
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-26 21:01:15 +0000

    fixed errors with show details page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,7 @@
 class MoviesController < ApplicationController
+
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -14,11 +17,9 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    # movie_params = params.require(:movie).permit(:title, :description, :image_url)
     
     @movie = Movie.new(movie_params)
 
@@ -32,13 +33,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-
-    # movie_params = params.require(:movie).permit(:title, :description, :image_url)
     
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -48,7 +45,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
 
     @movie.destroy
 
@@ -60,6 +56,10 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 
 end
